7f41362677b7106887ce902d50082e07c381dae7
Shary, STeve <stephen.shary@gmail.com>
2015-01-08 16:26:32 -0500

    Fixing that add function.

--- src/main/java/Adder.java
+++ src/main/java/Adder.java
@@ -1,9 +1,6 @@
 public class Adder {
 
   public static int add(int number1, int number2) {
-    if(number1 == 2 && number2 == 3) {
-      return 5;
-    }
-    return 6;
+    return number1 + number2;
   }
 }
--- src/test/java/AdderTest.java
+++ src/test/java/AdderTest.java
@@ -8,5 +8,6 @@ public class AdderTest {
   public void shouldAdd() {
     assertEquals(5, Adder.add(2, 3));
     assertEquals(6, Adder.add(3, 3));
+    assertEquals(7, Adder.add(3, 4));
   }
 }
